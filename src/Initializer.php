beb0e51d0cc12041dd569cfbe625707445db87f4
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-09-03 11:21:14 +0300

    Mods for internationalization

--- src/Initializer.php
+++ src/Initializer.php
@@ -23,14 +23,14 @@ use PanWPCore\Redux;
  */
 class Initializer extends \PanWPCore\Initializer {
 	protected function init() {
-		$this->Redux->addSection( 'Basic Field', 'basic', false, 'Basic field with no subsections', array(
+		$this->Redux->addSection( $this->I18n->__('Basic Field'), 'basic', false, 'Basic field with no subsections', array(
 			'fields' => array(
 				array(
 					'id'       => 'opt-text',
 					'type'     => 'text',
-					'title'    => __( 'Example Text', 'redux-framework-demo' ),
-					'desc'     => __( 'Example description.', 'redux-framework-demo' ),
-					'subtitle' => __( 'Example subtitle.', 'redux-framework-demo' ),
+					'title'    => $this->I18n->__( 'Example Text' ),
+					'desc'     => $this->I18n->__( 'Example description.' ),
+					'subtitle' => $this->I18n->__( 'Example subtitle.' ),
 					'hint'     => array(
 						'content' => 'This is a <b>hint</b> tool-tip for the text field.<br/><br/>Add any HTML based text you like here.',
 					)
@@ -38,32 +38,32 @@ class Initializer extends \PanWPCore\Initializer {
 			)
 		) );
 
-		$this->Redux->addSection( 'Basic Fields', 'basics' );
+		$this->Redux->addSection( $this->I18n->__('Basic Fields'), 'basics' );
 
-		$this->Redux->addSection( 'Text', 'opt-text-subsection', true,
+		$this->Redux->addSection( $this->I18n->__('Text'), 'opt-text-subsection', true,
 			'For full documentation on this field, visit: <a href="//docs.reduxframework.com/core/fields/text/" target="_blank">//docs.reduxframework.com/core/fields/text/</a>' );
 
 		$this->Redux->addField( 'opt-text-subsection',
 			array(
 				'id'       => 'text-example',
 				'type'     => 'text',
-				'title'    => __( 'Text Field', 'redux-framework-demo' ),
-				'subtitle' => __( 'Subtitle', 'redux-framework-demo' ),
-				'desc'     => __( 'Field Description', 'redux-framework-demo' ),
+				'title'    => $this->I18n->__( 'Text Field' ),
+				'subtitle' => $this->I18n->__( 'Subtitle' ),
+				'desc'     => $this->I18n->__( 'Field Description' ),
 				'default'  => 'Default Text',
 			)
 		);
 
-		$this->Redux->addSection( 'Text Area', 'opt-textarea-subsection', true,
+		$this->Redux->addSection( $this->I18n->__('Text Area'), 'opt-textarea-subsection', true,
 			'For full documentation on this field, visit: <a href="//docs.reduxframework.com/core/fields/text/" target="_blank">//docs.reduxframework.com/core/fields/textarea/</a>' );
 
 		$this->Redux->addField( 'opt-textarea-subsection',
 			array(
 				'id'       => 'textarea-example',
 				'type'     => 'textarea',
-				'title'    => __( 'Text Area Field', 'redux-framework-demo' ),
-				'subtitle' => __( 'Subtitle', 'redux-framework-demo' ),
-				'desc'     => __( 'Field Description', 'redux-framework-demo' ),
+				'title'    => $this->I18n->__( 'Text Area Field'),
+				'subtitle' => $this->I18n->__( 'Subtitle'),
+				'desc'     => $this->I18n->__( 'Field Description'),
 				'default'  => 'Default Text',
 			)
 		);
