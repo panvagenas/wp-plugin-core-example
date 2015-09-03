eb4d0af2a5cf07f4fcfbceb01ae57a79dbb2cb6e
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-09-04 02:28:11 +0300

    Options name can now be clarified via Options->optName property

---
+++ src/Options.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Project: pan-wp-core
+ * File: Options.php
+ * User: Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * Date: 4/9/2015
+ * Time: 2:25 πμ
+ * Since: TODO ${VERSION}
+ * Copyright: 2015 Panagiotis Vagenas
+ */
+
+namespace PanWPCoreExample;
+
+
+class Options extends \PanWPCore\Options{
+	protected $optName = 'example_options';
+}
